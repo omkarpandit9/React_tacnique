43ff952a56651da857c8616ddd32b40f839b18e5
omkarpandit9 <omkar.pandit9823@gmail.com>
2024-12-23 08:24:27 +0000

    Complete the Logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 function WeatherDashboard() {
   // Mock weather data
@@ -20,15 +20,44 @@ function WeatherDashboard() {
     },
   };
 
+  const [city, setCity] = useState(null);
+  const [cityMap, setCityMap] = useState([]);
+  const cityInput = useRef('')
+
+  const onSearch = (selectedCityName) => {
+    if (selectedCityName) {
+      const seletedCity = mockWeatherData[selectedCityName]
+      setCity(seletedCity)
+    } else {
+      console.log(cityInput.current.value)
+      const selectedCity = mockWeatherData[cityInput.current.value]
+      if (selectedCity) {
+        setCity(selectedCity)
+        console.log(cityMap.includes(cityInput.current.value))
+        if (!cityMap.includes(cityInput.current.value)) {
+          setCityMap((prevState) => {
+            return [...prevState, cityInput.current.value]
+          })
+        }
+      } else {
+        setCity(null)
+      }
+    }
+  }
+
   return (
     <div>
-      <input type="text" id="citySearch" placeholder="Search for a city..." />
-      <button id="searchButton">Search</button>
+      <input type="text" id="citySearch" ref={cityInput} placeholder="Search for a city..." />
+      <button id="searchButton" onClick={() => onSearch()}>Search</button>
+      <div>{
+        cityMap.map((ele) => <button style={{margin: '2px'}} id="searchButton" onClick={() => onSearch(ele)}>{ele}</button>)
+      }</div>
       <div id="weatherData">
-        <div>Temperature: </div>
-        <div>Humidity: </div>
-        <div>Wind Speed: </div>
-        <div>City not found.</div>
+        {city ? <>
+          <div>Temperature:{city.temperature} </div>
+          <div>Humidity: {city.humidity}</div>
+          <div>Wind Speed: {city.windSpeed}</div>
+        </> : <div>City not found.</div>}
       </div>
       <div id="previousSearches"></div>
     </div>
